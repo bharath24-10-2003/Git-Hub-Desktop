2e5e6a153167b6d8d6780de806a8663d192e7024
Tring-Bharath <bharath.a@tringapps.com>
2026-04-25 23:09:28 +0530

    Initial Commit

---
+++ Git Hub Desktop/Git_Hub_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Hub_DesktopApp.swift
+//  Git Hub Desktop
+//
+//  Created by Bharath on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Git_Hub_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
